9b4df1d65417798f711353710fb0f4f061c37732
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-07 12:54:35 +0400

    Add tip calculator program

---
+++ Easy_level/tip_calculator.py
@@ -0,0 +1,10 @@
+print("This program calculates the tip and total bill amount \n" \
+"The tip is 10 % of the bill amount. ")
+
+bill=float(input("Enter the bill amount: "))
+tip=0.1 * bill
+total=bill+ tip
+
+print  ("The amount of the tip is : " , tip)
+print ("The total bill amount is : " , total)
+
