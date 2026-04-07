ff4a89e51e6528062eac869aed52af5e19c322c3
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-07 12:58:53 +0400

    Add odd or even checker script

---
+++ Easy_level/odd_or_even_checker.py
@@ -0,0 +1,9 @@
+num=int(input("Enter the number: "))
+if num % 2 == 0 :
+    print("The number is even.")
+
+elif num %2 != 0 :
+    print("The number is odd.")
+
+else:
+    print("Invalid input!")
