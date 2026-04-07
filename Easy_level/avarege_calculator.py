9128722a00287e013970bee7390d3a73f521fc63
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-07 13:03:06 +0400

    Add average calculator for three numbers

---
+++ Easy_level/avarege_calculator.py
@@ -0,0 +1,9 @@
+# This program calculates the average of three numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+average = (num1 + num2 + num3) / 3
+
+print("The average is:", average)
